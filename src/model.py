93a9f8c8467f8729828577f7ac95bdb47db07c86
Paulo <pmssm@cin.ufpe.br>
2020-07-13 09:09:12 -0300

    mode

---
+++ src/model.py
@@ -0,0 +1,41 @@
+import sqlite3
+import re
+
+#Creating database
+conn = sqlite3.connect('/home/lluaki/Documents/projects/PulseRateAnalysis/database/PulseRateDB.sqlite')
+cur = conn.cursor()
+
+cur.execute("DROP TABLE IF EXISTS Person")
+cur.execute(''' CREATE TABLE Person (
+  id INTEGER PRIMARY KEY,
+  active INTEGER,
+  rest INTEGER,
+  smoke BOOLEAN,
+  sex BOOLEAN,
+  exercise INTEGER,
+  height INTEGER,
+  weight INTEGER,
+  variation FLOAT
+  )  ''')
+
+fh = open("/home/lluaki/Documents/projects/PulseRateAnalysis/assets/Pulse.csv")
+
+def strip(x):
+  return x.strip('"')
+
+for line in fh:
+  line = line.split(',')
+
+  if line[0] == '""': continue
+  
+  line = map(lambda x: int(x) if not(x.startswith('"')) else x, line)
+  line = list(line)
+  variation = (line[1] - line[2])/line[2]*100
+  _id = re.findall("\d+", line[0])
+  
+  cur.execute(''' 
+      INSERT INTO Person (id, active, rest, smoke, sex, exercise, height, weight, variation)
+      VALUES (?,?,?,?,?,?,?,?,?)           
+  ''', (int(_id[0]), line[1], line[2], line[3], line[4], line[5], line[6], line[7], variation))
+  conn.commit()
+cur.close()
